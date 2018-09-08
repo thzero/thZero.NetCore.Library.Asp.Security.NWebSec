84f82114b09c41cb84ab96bdaecf52694d36e863
thZero.com <development@thzero.com>
2018-09-08 12:18:27 -0500

    master

---
+++ ServiceHttpSecurityNWebSec.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Builder;
+
+namespace thZero.Services
+{
+    public class ServiceHttpSecurityNWebSec : IServiceHttpSecurity
+    {
+        public void InitializeSsl(IApplicationBuilder app)
+        {
+            app.UseHsts(opts => opts.AllResponses());
+        }
+
+        public void InitializeStaticPost(IApplicationBuilder app)
+        {
+            app.UseXfo(xfo => xfo.Deny());
+            app.UseRedirectValidation(); //Register this earlier if there's middleware that might redirect.
+            app.UseNoCacheHttpHeaders();
+        }
+
+        public void InitializeStaticPre(IApplicationBuilder app)
+        {
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXDownloadOptions();
+            app.UseXXssProtection(opts => opts.Enabled());
+        }
+    }
+}

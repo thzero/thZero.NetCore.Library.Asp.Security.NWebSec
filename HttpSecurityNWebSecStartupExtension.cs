c790f23c15e6c6fbd48776607e07fe42a0d76492
thZero.com <development@thzero.com>
2018-09-17 08:42:19 -0500

    startup extension changes

--- HttpSecurityNWebSecStartupExtension.cs
+++ HttpSecurityNWebSecStartupExtension.cs
@@ -1,35 +1,54 @@
 ﻿using System;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace thZero.AspNetCore
 {
     public class HttpSecurityNWebSecStartupExtension : IStartupExtension
     {
         #region Public Methods
-        public void InitializeMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void InitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
         }
 
-        public void InitializeMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void InitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
         }
 
-        public void InitializeSsl(IApplicationBuilder app)
+        public virtual void InitializeMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        {
+        }
+
+        public virtual void InitializeMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        {
+        }
+
+        public virtual void InitializeRoutesBuilderPost(IRouteBuilder routes)
+        {
+        }
+
+        public virtual void InitializeRoutesBuilderPre(IRouteBuilder routes)
+        {
+        }
+
+        public virtual void InitializeSsl(IApplicationBuilder app)
         {
             app.UseHsts(opts => opts.AllResponses());
         }
 
-        public void InitializeStaticPost(IApplicationBuilder app)
+        public virtual void InitializeStaticPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
             app.UseXfo(xfo => xfo.Deny());
             app.UseRedirectValidation(); //Register this earlier if there's middleware that might redirect.
             app.UseNoCacheHttpHeaders();
         }
 
-        public void InitializeStaticPre(IApplicationBuilder app)
+        public virtual void InitializeStaticPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
             app.UseXContentTypeOptions();
             app.UseReferrerPolicy(opts => opts.NoReferrer());

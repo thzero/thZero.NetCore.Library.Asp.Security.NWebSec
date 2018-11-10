6efe7e4bc0524b7c3294a7f651697038ca5015c5
thZero.com <development@thzero.com>
2018-11-10 10:19:54 -0600

    Updates to extension

--- HttpSecurityNWebSecStartupExtension.cs
+++ HttpSecurityNWebSecStartupExtension.cs
@@ -12,48 +12,56 @@ namespace thZero.AspNetCore
     public class HttpSecurityNWebSecStartupExtension : IStartupExtension
     {
         #region Public Methods
-        public virtual void InitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigureInitializePost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
-        public virtual void InitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigureInitializePre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
-        public virtual void InitializeMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
         }
 
-        public virtual void InitializeMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
         }
 
-        public virtual void InitializeRoutesBuilderPost(IRouteBuilder routes)
+        public virtual void ConfigureInitializeRoutesBuilderPost(IRouteBuilder routes)
         {
         }
 
-        public virtual void InitializeRoutesBuilderPre(IRouteBuilder routes)
+        public virtual void ConfigureInitializeRoutesBuilderPre(IRouteBuilder routes)
         {
         }
 
-        public virtual void InitializeSsl(IApplicationBuilder app)
+        public virtual void ConfigureInitializeSsl(IApplicationBuilder app)
         {
             app.UseHsts(opts => opts.AllResponses());
         }
 
-        public virtual void InitializeStaticPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
+        public virtual void ConfigureInitializeStaticPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
             app.UseXfo(xfo => xfo.Deny());
             app.UseRedirectValidation(); //Register this earlier if there's middleware that might redirect.
             app.UseNoCacheHttpHeaders();
         }
 
-        public virtual void InitializeStaticPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
+        public virtual void ConfigureInitializeStaticPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
             app.UseXContentTypeOptions();
             app.UseReferrerPolicy(opts => opts.NoReferrer());
             app.UseXDownloadOptions();
             app.UseXXssProtection(opts => opts.Enabled());
+        }
+
+        public virtual void ConfigureServicesMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        {
+        }
+
+        public virtual void ConfigureServicesMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        {
         }
         #endregion
     }

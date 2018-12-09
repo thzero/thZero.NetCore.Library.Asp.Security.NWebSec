c3aae19836af978cba11331c9570707bd2c1442a
thZero.com <development@thzero.com>
2018-12-09 08:16:28 -0600

    Nuget updates, update to netcore2.2

--- HttpSecurityNWebSecStartupExtension.cs
+++ HttpSecurityNWebSecStartupExtension.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,11 +13,11 @@ namespace thZero.AspNetCore
     public class HttpSecurityNWebSecStartupExtension : IStartupExtension
     {
         #region Public Methods
-        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
-        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
@@ -28,11 +29,11 @@ namespace thZero.AspNetCore
         {
         }
 
-        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
-        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
         {
         }
 
@@ -66,19 +67,43 @@ namespace thZero.AspNetCore
             app.UseXXssProtection(opts => opts.Enabled());
         }
 
-        public virtual void ConfigureServicesPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesPost(IServiceCollection services, IConfiguration configuration)
         {
         }
 
-        public virtual void ConfigureServicesPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesPre(IServiceCollection services, IConfiguration configuration)
         {
         }
 
-        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcBuilder builder)
         {
         }
 
-        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfigurationRoot configuration)
+        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcBuilder builder)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcCoreBuilder builder)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcCoreBuilder builder)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcOptionsPost(MvcOptions options)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcOptionsPre(MvcOptions options)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfiguration configuration)
+        {
+        }
+
+        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfiguration configuration)
         {
         }
         #endregion

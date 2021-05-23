c5d5d765e1a2057e6add13b72fbe0c7dd5f3a138
thzero <thzero@users.noreply.github.com>
2021-05-23 10:08:27 -0500

    code analysis updates

--- HttpSecurityNWebSecStartupExtension.cs
+++ HttpSecurityNWebSecStartupExtension.cs
@@ -17,10 +17,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
  * ------------------------------------------------------------------------- */
 
-using System;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using System;
 
 namespace thZero.AspNetCore
 {
